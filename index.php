184a080a600b1e9b1027729a8e5a9916bd2d4de0
AcAkb <566254@edu.rocmn.nl>
2023-01-24 10:47:29 +0100

    .

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
-	include "data/private/database.php"
+	ini_set("display_errors", 1);
+	ini_set("display_startup_errors", 1);
+	error_reporting(E_ALL);
+
+	include "data/private/database.php";
+
+	if(array_key_exists("button_one", $_POST)) {
+		button_one();
+	};
+	function button_one() {
+		echo "This is a button";
+	};
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,8 +20,23 @@
 	<meta name="robots" content="noindex, nofollow">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="data/css/main.css">
 	<title>Document</title>
 </head>
 <body>
+	<header>
+		<div class="container">
+			<form method="post">
+				<input type="submit" name="button_one" value="Button" class="button_one">
+			</form>
+		</div>
+	</header>
+	<div class="viewport">
+		<div class="main"></div>
+	</div>
+	<footer>
+		<h5>&copy; Copyright 2023-2024</h5>
+	</footer>
 </body>
+<script type="text/javascript" src="data/js/main.js"></script>
 </html>
