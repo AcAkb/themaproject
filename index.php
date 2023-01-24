d7547c996563884cb7063aa99de033a9a36b9273
AcAkb <566254@edu.rocmn.nl>
2023-01-24 13:20:28 +0100

    .

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
 	function button_one() {
 		echo "FACK YOU MEOW";
 	};
+	if(array_key_exists("button_two", $_POST)) {
+		button_one();
+	};
+	function button_two() {
+		echo "FACK YOU MEOW 2";
+	};
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,12 +37,12 @@
 			<form method="post">
 				<input type="submit" name="button_one" value="Add Item" class="button_one">
 			</form>
-			<form action="index.php" method="post">
+			<form action="index.php" method="get">
 				<input type="text" placeholder="Zoeken.." name="search" class="searchbar">
 				<button type="submit" class="icon_parent"><i class="fa fa-search icon"></i></button>
 			</form>
 			<form method="post">
-				<input type="submit" name="button_two" value="Contact" class="button_two">
+				<input type="submit" name="button_two" value="" class="button_two">
 			</form>
 		</div>
 	</header>
@@ -54,7 +60,7 @@
 				$result = $conn->query($sql);
 				if ($result->num_rows > 0) {
 					while($row = $result->fetch_assoc()) {
-						echo "<div class='data'>";
+						echo "<div class='data standard'>";
 							echo "<div class='item_name'>".$row["name"]."</div>";
 							echo "<div class='item_mail'>".$row["mail"]."</div>";
 							echo "<div class='item_phone'>".$row["phone"]."</div>";
@@ -65,6 +71,40 @@
 				} else {
 					echo "Geen resultaat";
 				}
+					if(!empty($_GET["search"])) {
+
+				$query = $_GET["search"]; 
+				$min_length = 3;
+				if(strlen($query) >= $min_length) {
+					$query = htmlspecialchars($query);
+					$query = mysqli_real_escape_string($conn, $query);
+					$raw_results = mysqli_query($conn, "SELECT * FROM data WHERE (`name` LIKE '%".$query."%') OR (`name` LIKE '%".$query."%')") or die(mysqli_error());
+					if(mysqli_num_rows($raw_results) > 0) {
+						while($row = mysqli_fetch_array($raw_results)){
+							print(
+							"
+								<script>
+									$('.standard').hide();
+								</script>
+							"
+							);
+							echo "<div class='data'>";
+								echo "<div class='item_name'>".$row["name"]."</div>";
+								echo "<div class='item_mail'>".$row["mail"]."</div>";
+								echo "<div class='item_phone'>".$row["phone"]."</div>";
+								echo "<div class='item_information'>".$row["information"]."</div>";
+								echo "<div class='item_location'>".$row["location"]."</div>";
+							echo "</div>";
+						}	
+					}
+					else {
+						echo "Geen resultaat";
+					}
+				}
+				else {
+					echo "Minimum lengte is ".$min_length;
+				};
+			};
 			?>
 		</div>
 	</div>
