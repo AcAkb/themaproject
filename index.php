a4577ae92f57e0609310705d3409bb36ecb45a73
AcAkb <566254@edu.rocmn.nl>
2023-01-24 10:25:38 +0100

    .

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	include "data/private/database.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="content-type" content="text/html">
+	<meta name="robots" content="noindex, nofollow">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" crossorigin="anonymous"></script>
+	<title>Document</title>
+</head>
+<body>
+</body>
+</html>
